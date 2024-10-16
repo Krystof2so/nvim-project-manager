f12cf05343043480c84ec42b94d3f7b8effd1b79
Krystof2so <christo.2so@gmail.com>
2024-10-16 14:04:39 +0200

    (fix) create project
    with Telescope

--- lua/project_manager/create.lua
+++ lua/project_manager/create.lua
@@ -9,49 +9,67 @@
 -- *************************************************************************
 
 local M = {}
+local telescope_builtin = require('telescope.builtin')
 
--- Fonction pour créer un nouveau projet dans un répertoire sélectionné via Telescope
-function M.create_project_with_telescope()
-    -- Utiliser Telescope pour choisir le répertoire cible
-    require('telescope.builtin').find_files({
-        prompt_title = "Choisissez le répertoire cible",
-        cwd = vim.fn.expand("~"),  -- Commence dans le répertoire home
-        find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git" }, -- Limite la recherche aux répertoires
+-- *********************************************
+-- * Fonction pour sélectionner un répertoire  *
+-- * avec Telescope                           *
+-- *********************************************
+local function choose_directory_with_telescope(callback)
+    telescope_builtin.find_files({
+        prompt_title = "Sélectionnez un répertoire",
+        cwd = vim.fn.getcwd(),
+        find_command = { 'fd', '--type', 'd' },  -- Rechercher uniquement des répertoires
         attach_mappings = function(prompt_bufnr, map)
             local actions = require('telescope.actions')
             local action_state = require('telescope.actions.state')
 
-            -- Fonction pour confirmer la sélection et créer un sous-répertoire
-            local function confirm_selection()
+            -- Quand l'utilisateur sélectionne un répertoire
+            map('i', '<CR>', function()
                 local selection = action_state.get_selected_entry()
                 actions.close(prompt_bufnr)
 
-                -- Obtenir le chemin sélectionné
-                local selected_directory = selection.path or selection[1]
-
-                -- Demander le nom du projet
-                local project_name = vim.fn.input("Nom du projet: ")
-
-                if project_name == "" then
-                    vim.notify("Nom du projet non spécifié, opération annulée.", vim.log.levels.WARN)
-                    return
+                if selection and selection.path then
+                    callback(selection.path)  -- Exécute le callback avec le répertoire sélectionné
+                else
+                    vim.notify("Répertoire non valide.", vim.log.levels.ERROR)
                 end
-
-                -- Construire le chemin complet du nouveau répertoire
-                local project_path = selected_directory .. "/" .. project_name
-
-                -- Créer le répertoire
-                vim.fn.mkdir(project_path, "p")
-                vim.notify("Projet créé dans : " .. project_path)
-            end
-
-            -- Mapper la touche Enter pour confirmer la sélection
-            map('i', '<CR>', confirm_selection)
-            map('n', '<CR>', confirm_selection)
+            end)
 
             return true
         end
     })
+end
+
+-- ******************************************
+-- * Fonction pour saisir le nom du projet  *
+-- * dans le terminal                      *
+-- ******************************************
+local function prompt_for_project_name()
+    local project_name = vim.fn.input("Entrez le nom du projet: ")
+
+    if project_name == "" then
+        vim.notify("Nom du projet vide, opération annulée.", vim.log.levels.WARN)
+        return nil
+    end
+
+    return project_name
+end
+
+-- ******************************************
+-- * Fonction pour créer un nouveau projet  *
+-- * en utilisant Telescope                *
+-- ******************************************
+function M.create_project_with_telescope()
+    choose_directory_with_telescope(function(selected_directory)
+        local project_name = prompt_for_project_name()
+
+        if project_name then
+            local project_path = selected_directory .. "/" .. project_name
+            vim.fn.mkdir(project_path, "p")  -- Crée le répertoire du projet
+            vim.notify("Projet créé dans : " .. project_path)
+        end
+    end)
 end
 
 return M
--- plugin/project_manager.lua
+++ plugin/project_manager.lua
@@ -2,15 +2,11 @@
 -- Chargé par Neovim une fois installé
 -- configure les commandes disponibles pour le plugin.
 
--- Charge le module principal
-local project_manager = require('project_manager')
 
 -- Ajouter une commande pour créer un projet avec Telescope
 vim.api.nvim_create_user_command(
-    'CreateProjectWithTelescope',
-    function()
-        project_manager.create_project_with_telescope()
-    end,
-    {}
+    'CreateProject',
+    function() require('project_manager.create').create_project_with_telescope() end,
+    { desc = 'Créer un projet via Telescope' }
 )
 
