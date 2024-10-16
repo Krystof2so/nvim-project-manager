9876428563942aa9839da0a75f905cb862b1dbf8
Krystof2so <christo.2so@gmail.com>
2024-10-16 13:47:09 +0200

    (fix) add command
    :CreateProjectWithTelescope

--- plugin/project_manager.lua
+++ plugin/project_manager.lua
@@ -5,11 +5,12 @@
 -- Charge le module principal
 local project_manager = require('project_manager')
 
--- Commandes Neovim pour créer et ouvrir un projet :
-vim.api.nvim_create_user_command('ProjectManagerCreate', project_manager.create_project, { nargs = 0 })
-vim.api.nvim_create_user_command('ProjectManagerOpen', project_manager.open_project, { nargs = 0 })
-
--- Configuration des keymaps
-vim.api.nvim_set_keymap('n', '<leader>cp', ':CreateProject<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>op', ':OpenProject<CR>', { noremap = true, silent = true })
+-- Ajouter une commande pour créer un projet avec Telescope
+vim.api.nvim_create_user_command(
+    'CreateProjectWithTelescope',
+    function()
+        project_manager.create_project_with_telescope()
+    end,
+    {}
+)
 
